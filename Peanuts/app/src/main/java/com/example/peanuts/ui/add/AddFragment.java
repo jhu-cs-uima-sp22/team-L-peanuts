32ff1bd6087964f24a3895f3921203aa466e1008
kaylalaufer <klaufer1@jhu.edu>
2022-04-04 17:17:54 -0400

    Added design elements to add food page

--- Peanuts/app/src/main/java/com/example/peanuts/ui/add/AddFragment.java
+++ Peanuts/app/src/main/java/com/example/peanuts/ui/add/AddFragment.java
@@ -2,28 +2,56 @@ package com.example.peanuts.ui.add;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.peanuts.R;
 import com.example.peanuts.databinding.FragmentAddBinding;
+import androidx.recyclerview.widget.RecyclerView;
 import com.example.peanuts.databinding.FragmentNotificationsBinding;
 import com.example.peanuts.ui.notifications.NotificationsViewModel;
+
+import java.util.List;
 
 public class AddFragment extends Fragment {
 
     private FragmentAddBinding binding;
+    private boolean mTwoPane;
+    private String userId;
+    private List<String> mItems;
+    private RecyclerView.RecyclerListener mAdapt;
+
+   /* @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Activity activity = this.getActivity();
+        //not sure what to do with recyclerView
+
+        mItems.add("Peanuts");
+        mItems.add("Dairy");
+        mItems.add("Tree nuts");
+        mItems.add("Shellfish");
+        mItems.add("Seafood");
+        mItems.add("Strawberry");
+        mItems.add("Soy");
+        mItems.add("Gluten");
+        mItems.add("Eggs");
+    }*/
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
         AddViewModel addViewModel =
                 new ViewModelProvider(this).get(AddViewModel.class);
 
@@ -31,7 +59,7 @@ public class AddFragment extends Fragment {
         View root = binding.getRoot();
 
         final TextView textView = binding.textAdd;
-        addViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
         return root;
     }
 
@@ -40,5 +68,6 @@ public class AddFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
 
 }
