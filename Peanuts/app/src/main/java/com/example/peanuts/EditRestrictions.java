0e4d1dadf014a62bda5bede1c1c0b4d504a844b1
jwfung <59128478+jwfung@users.noreply.github.com>
2022-04-06 16:00:29 -0400

    edit restrictions WIP

--- Peanuts/app/src/main/java/com/example/peanuts/EditRestrictions.java
+++ Peanuts/app/src/main/java/com/example/peanuts/EditRestrictions.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class EditRestrictions extends AppCompatActivity {
+
+    protected ArrayList<Item> restrictions;
+    protected ItemAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_restrictions);
+
+        // create temp ArrayList of items
+        restrictions = new ArrayList<Item>();
+        restrictions.add(new Item("peanuts", true));
+        restrictions.add(new Item("dairy", false));
+        restrictions.add(new Item("seafood", true));
     }
 }
