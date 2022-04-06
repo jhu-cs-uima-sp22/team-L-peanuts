7626cc9b6c93787fa65675666683f795b3d0f850
jwfung <59128478+jwfung@users.noreply.github.com>
2022-04-06 13:03:22 -0400

    connect screens to empty Edit Restrictions

---
+++ Peanuts/app/src/main/java/com/example/peanuts/EditRestrictions.java
@@ -0,0 +1,14 @@
+package com.example.peanuts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditRestrictions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_restrictions);
+    }
+}
