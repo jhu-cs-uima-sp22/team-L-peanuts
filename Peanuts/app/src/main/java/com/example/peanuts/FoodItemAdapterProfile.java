322d795eb096ec0320fcf6bf6a73bd7d2d98a604
KiwiThePoodle <42877043+KiwiThePoodle@users.noreply.github.com>
2022-04-22 02:33:58 -0400

    Used database to populate foods on profile and edit foods screen

--- Peanuts/app/src/main/java/com/example/peanuts/FoodItemAdapterProfile.java
+++ Peanuts/app/src/main/java/com/example/peanuts/FoodItemAdapterProfile.java
@@ -1,7 +1,12 @@
 package com.example.peanuts;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -46,7 +51,6 @@ public class FoodItemAdapterProfile extends ArrayAdapter<FoodItem> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         LinearLayout foodItemView;
-        FoodItem it = getItem(position);
 
         if (convertView == null) {
             foodItemView = new LinearLayout(getContext());
@@ -57,23 +61,44 @@ public class FoodItemAdapterProfile extends ArrayAdapter<FoodItem> {
             foodItemView = (LinearLayout) convertView;
         }
 
-        myRefForFoods.child(user).child("" + position).child("imageUri").addValueEventListener(new ValueEventListener() {
+        myRefForFoods.child(user).child("" + position).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 Log.d("debug", "Position: " + position );
                 Log.d("debug", "Data: " + dataSnapshot);
 
-                imageUri = (String) dataSnapshot.getValue();
+                imageUri = (String) dataSnapshot.child("imageUri").getValue();
                 ImageView foodButton = (ImageView) foodItemView.findViewById(R.id.food_item_image_profile);
-                foodButton.setImageURI(Uri.parse(imageUri));
+                if (imageUri != null) {
+                    foodButton.setImageURI(Uri.parse(imageUri));
+                }
+                foodButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Context context = getContext();
+                        Intent intent = new Intent(context, FoodDetail.class);
+                        String name = (String) dataSnapshot.child("name").getValue();
+                        ArrayList<String> list = (ArrayList<String>) dataSnapshot.child("allergens").getValue();
+                        boolean[] restrictions = new boolean[12];
+                        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+                        for (int i = 0; i < 12; i++) {
+                            if (list.contains(preferences.getString("" + i, ""))) {
+                                restrictions[i] = true;
+                            }
+                        }
+                        intent.putExtra("name", name);
+                        intent.putExtra("restrictions", restrictions);
+                        intent.putExtra("image", imageUri);
+                        context.startActivity(intent);
+                        notifyDataSetChanged();
+                    }
+                });
             }
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
                 Log.d("retrieve_fail", databaseError.toString());
             }
         });
-
-
         return foodItemView;
     }
 
